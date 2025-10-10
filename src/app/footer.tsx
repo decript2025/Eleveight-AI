4d574d0d978af6a7b2edf33ff7a88d40825cd404
Harut Manukyan <harut.manukyan@decript.tech>
2025-10-10 20:44:04 +0400

    fix: reposition ReservationDialog in footer for better layout on small screens

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -6,12 +6,11 @@ import { ReservationDialog } from './ReservationDialog';
 export default function Footer() {
   return (
     <footer className="w-full h-[200px] bg-[#1E1D21] flex-shrink-0 relative text-white">
-      <div className="absolute top-5 right-5 md:hidden">
-        <ReservationDialog />
-      </div>
-      
-      <div className="w-full h-full flex flex-col justify-center items-center pt-10 sm:pt-0">
-        <div className="flex flex-col items-center gap-10">
+      <div className="w-full h-full flex flex-col justify-center items-center">
+        <div className="flex flex-col items-center gap-8">
+          <div className="md:hidden pt-5">
+            <ReservationDialog />
+          </div>
           <Link 
             href="/faq" 
             className="text-xl font-bold hover:opacity-80 transition-opacity duration-200"
